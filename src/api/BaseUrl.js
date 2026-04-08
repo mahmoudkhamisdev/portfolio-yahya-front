d31353dbd772b585b9fcd0ccbc0fd56c75d16a1f
mahmoudkhamisdev <dc.khamis10@gmail.com>
2026-04-09 00:16:29 +0200

    add .env

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // export const urlWeb = "http://127.0.0.1:8080";
-export const urlWeb = "https://portfolio-yaya-backend.vercel.app";
+export const urlWeb = process.env.REACT_APP_BASE_URL;
 const baseUrl = axios.create({ baseURL: urlWeb });
 
 export default baseUrl;
